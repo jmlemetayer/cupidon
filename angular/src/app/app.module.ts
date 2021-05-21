7f52404aa0420c8f82dede4a35bd04f772a468d9
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2021-05-22 00:28:35 +0200

    angular: root: add toolbar

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './root/root.component';
 import { FilesComponent } from './files/files.component';
@@ -15,6 +19,9 @@ import { FilesComponent } from './files/files.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [RootComponent]
